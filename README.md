0efefc3b4801163d4f53164e0d30db29b8d407e1
btcm31 <congminhabcd@gmail.com>
2021-09-05 16:44:37 +0700

    improve

--- README.md
+++ README.md
@@ -1,1 +1,113 @@
-## Basic Neural Network
+## Neural Network
+
+### Activation
+
+#### Sigmoid
+
+- Defining:
+  $\sigma(x) = \dfrac{1}{1 + e^{-x}}$
+- Derivative:
+  $\dfrac{\partial}{\partial x}\sigma(x) = \sigma(x)(1 - \sigma(x))$
+
+- Cons:
+  - Not a zero centric function.
+  - Suffers with gradient vanishing.
+  - Output of values which are far away from centroid is close to zero.
+  - Computationally expensive because it has to calculate exponential value in function.
+- Cons:
+  - Smooth gradient, preventing “jumps” in output values.
+  - Output values bound between 0 and 1, normalizing the output of each neuron.
+
+#### Tanh
+
+- Defining:
+  $\sigma(x) = \dfrac{e^{x} - e^{-x}}{e^{x} + e^{-x}}$
+- Derivative:
+  $\dfrac{\partial}{\partial x}\sigma(x) = (1 - \sigma^2(x))$
+
+- Cons:
+  - more computation expensive than sigmoid function.
+  - suffers with gradient vanishing.
+  - output of values which are far away from centroid is close to zero.
+- Pros:
+  - have all advantages of sigmoid function and it also a zero centric function.
+
+#### ReLU
+
+- Defining:
+  $\sigma(x) = \mathrm{max}(0, x)$
+- Derivative:
+  $\dfrac{\partial}{\partial x}\sigma(x)=
+    \begin{cases}
+    0 & \text{if } x < 0 \\
+    1 & \text{if } x > 0 \\
+    \end{cases}
+  $
+
+- Cons:
+  - No matter what for negative values neuron is completely inactive.
+  - Non zero centric function.
+- Pros:
+  - No gradient vanishing
+  - Derivative is constant
+  - Less computation expensive
+
+#### PReLU
+
+- Defining:
+  $\sigma(x) = 
+    \begin{Bmatrix} x & x >= 0 \\
+    \alpha x & x < 0 \\
+    \end{Bmatrix}
+  $
+- Derivative:
+  $\dfrac{\partial}{\partial x}\sigma(x)=
+    \begin{cases}
+    \alpha & \text{if } x < 0 \\
+    1 & \text{if } x > 0 \\
+    \end{cases}
+  $
+
+- Cons:
+  - it can’t be used for the complex Classification. It lags behind the Sigmoid and Tanh for some of the use cases.
+- Pros:
+  - are one attempt to fix the “dying ReLU” problem by having a small negative slope (of 0.01, or so).
+
+#### ELU
+
+- Defining:
+  $\sigma(z) = 
+    \begin{Bmatrix} z & z >= 0 \\
+    α.( e^z – 1) & z < 0\\
+    \end{Bmatrix}
+  $
+- Derivative:
+  $\dfrac{\partial}{\partial x}\sigma(x) =
+    \begin{Bmatrix} 1 & z>=0 \\
+    \alpha.e^z & z<0 \\
+    \end{Bmatrix}
+  $
+
+- Cons: For x > 0, it can blow up the activation with the output range of [0, inf].
+- Pros:
+  - help alleviate the vanishing gradient
+  - it has a nonzero gradient when x < 0, avoids the dying units issues.
+  - is smooth every where, helps speed up GD
+
+#### Speed: elu > prelu > relu > tanh > logistic
+
+#### Softmax
+
+- Defining:
+  $\sigma(j)=\frac{\exp(\mathbf{w}_j^\top \mathbf x)}{\sum_{k=1}^K \exp(\mathbf{w}_k^\top\mathbf x)}=\frac{\exp(z_j)}{\sum_{k=1}^K \exp(z_k)}$
+
+- Derivative:
+  $\frac{\partial}{\partial \mathbf{w}_i}\sigma(j)=\sigma(j)\left(\delta_{ij}-\sigma(i)\right)\mathbf x$
+
+<p align="center">
+<img src="img/softmax.png">
+</p>
+
+### Run
+
+- `python neu.py`
--- neu.py
+++ neu.py
@@ -1,26 +1,41 @@
 import numpy as np
 import pandas as pd
 import time
+from sklearn.datasets import fetch_openml
 
 class NeuralNetwork:
     """
     activation: Activation function for the hidden layer.
     lr: Learning rate schedule for weight updates.
+    batch_size: Size of minibatches for stochastic optimizers
+    alpha_activation: Only used when activation='prelu' or 'elu'
+    alpha: L2 penalty (regularization term) parameter.
     """
-    def __init__(self, layer_shape = [], lr=0.1, activation='logistic'):
+    def __init__(self, layer_shape = [], lr=0.1, activation='logistic', alpha_activation=0.01, batch_size='auto', alpha = 0.0001):
         if not layer_shape:
             raise ValueError("layer_shape expected non-empty list")
         elif len(layer_shape) < 3:
             raise ValueError("layer_shape expected at least 3 layers (input layer | hidden layers | output layer)")
         if type(lr) is not float:
             raise TypeError("lr expected float type, not %s" % (type(lr)))
-        if activation not in ['relu', 'logistic', 'tanh']:
+
+        if activation not in ['relu', 'logistic', 'tanh', 'prelu', 'elu']:
             self.activation = 'logistic'
         else:
             self.activation = activation
 
+        self.batch_size = batch_size
+        self.alpha = alpha 
+
+        #avtivation function for outputlayer
+        if layer_shape[-1] in [1, 2]:
+            self.out_activation = 'sigmoid'
+        else:
+            self.out_activation = 'softmax'
+
         self.layers = layer_shape
-        self.lr = lr
+        self.learning_rate = lr
+        self.alpha_activation = alpha_activation
 
         #random initialization weights and bias
         self.__W = []
@@ -31,56 +46,78 @@ class NeuralNetwork:
             self.__W.append(Wi)
             self.__b.append(bi)
 
-    def __activation(self, x):
+    def __hidden_activation(self, x):
         if self.activation == 'logistic':
             return 1 / (1 + np.exp(-x))
         elif self.activation == 'relu':
             return np.maximum(0, x)
         elif self.activation == 'tanh':
             return (np.exp(x) - np.exp(-x)) / (np.exp(x) + np.exp(-x))
+        elif self.activation == 'prelu':
+            x[x[:]<0] *= self.alpha_activation
+            return x
+        elif self.activation == 'elu':
+            x[x[:]<0] = self.alpha_activation * (np.exp(x[x[:]<0]) - 1)
+            return x
 
-    def __activation_derivative(self, x):
+    def __hidden_activation_derivative(self, x):
         if self.activation == 'logistic':
             return x * (1 - x)
         elif self.activation == 'relu':
-            x[x[:]>0]=1
-            x[x[:]<=0]=0
+            x[x[:]>=0]=1
+            x[x[:]<0]=0
             return x
         elif self.activation == 'tanh':
             return 1 - x**2
-            
-    def __sigmoid(self, x):
-        return 1 / (1 + np.exp(-x))
-    def __sigmoid_derivative(self, x):
-        return x * (1-x)
+        elif self.activation == 'prelu':
+            x[x[:]<0] = self.alpha_activation
+            x[x[:]>=0]=1
+            return x
+        elif self.activation == 'elu':
+            x[x[:]>0] = 1
+            x[x[:]<0] += self.alpha_activation
+            return np.maximum(0.01 * x, x)
+
+    def __output_activation(self, x):
+        if self.out_activation == 'sigmoid':
+            return 1 / (1 + np.exp(-x))
+        elif self.out_activation == 'softmax':
+            return np.exp(x) / np.sum(np.exp(x))
+
+    def __output_activation_derivative(self,x):
+        if self.out_activation == 'sigmoid':
+            return x * (1-x)
+        elif self.out_activation == 'softmax':
+            return np.diagflat(x) - np.dot(x, x.T)
 
     def __fit_partial(self, X, y):
         num_of_samples = X.shape[0]
-        A = [X] 
+        A = [X]
         #feedfoward
         a_i = A[-1]
         for i in range(len(self.layers)-1):
             #sigmoid  function for output layer
             if i == len(self.layers)-2:
-                a_i = self.__sigmoid(np.dot(a_i, self.__W[i]) + self.__b[i].T)
+                a_i = self.__output_activation(np.dot(a_i, self.__W[i]) + self.__b[i].T)
             #activation function for hidden layers
             else:
-                a_i = self.__activation(np.dot(a_i, self.__W[i]) + self.__b[i].T)
+                a_i = self.__hidden_activation(np.dot(a_i, self.__W[i]) + self.__b[i].T)
             A.append(a_i)
 
         #backpropagation
-        dJ = [-(y/A[-1] - (1-y)/(1-A[-1]))]
+        epsilon = 0.00001 #a number to advoid division by zero
+        dJ = [-(y/(A[-1] + epsilon) - (1-y)/(1-A[-1] + epsilon))]
         dW = []
         db = []
         for i in reversed(range(len(self.layers)-1)):
             if i == len(self.layers)-2:
-                dw_ = np.dot(A[i].T, dJ[-1] * self.__sigmoid_derivative(A[i+1]))
-                db_ = np.sum(dJ[-1] * self.__sigmoid_derivative(A[i+1]), axis=0).reshape(-1,1)
-                dj_ = np.dot(dJ[-1] * self.__sigmoid_derivative(A[i+1]), self.__W[i].T)
+                dw_ = np.dot(A[i].T, dJ[-1] * self.__output_activation_derivative(A[i+1]))
+                db_ = np.sum(dJ[-1] * self.__output_activation_derivative(A[i+1]), axis=0).reshape(-1,1)
+                dj_ = np.dot(dJ[-1] * self.__output_activation_derivative(A[i+1]), self.__W[i].T)
             else:
-                dw_ = np.dot(A[i].T, dJ[-1] * self.__activation_derivative(A[i+1]))
-                db_ = np.sum(dJ[-1] * self.__activation_derivative(A[i+1]), axis=0).reshape(-1,1)
-                dj_ = np.dot(dJ[-1] * self.__activation_derivative(A[i+1]), self.__W[i].T)
+                dw_ = np.dot(A[i].T, dJ[-1] * self.__hidden_activation_derivative(A[i+1]))
+                db_ = np.sum(dJ[-1] * self.__hidden_activation_derivative(A[i+1]), axis=0).reshape(-1,1)
+                dj_ = np.dot(dJ[-1] * self.__hidden_activation_derivative(A[i+1]), self.__W[i].T)
             dJ.append(dj_)
             dW.append(dw_)
             db.append(db_)
@@ -89,11 +126,13 @@ class NeuralNetwork:
         db = db[::-1]
 
         for i in range(len(self.layers)-1):
-            self.__W[i] -= self.lr * dW[i]
-            self.__b[i] -= self.lr * db[i]
+            self.__W[i] = self.__W[i]*(1 - self.learning_rate * self.alpha / num_of_samples) - self.learning_rate * dW[i]
+            self.__b[i] -= self.learning_rate * db[i]
 
     def fit(self, X, y, epochs, batch_size):
         num_of_samples = X.shape[0]
+        if self.batch_size == 'auto':
+            batch_size = min(200, num_of_samples)
         if batch_size > num_of_samples:
             batch_size = num_of_samples
 
@@ -118,16 +157,21 @@ class NeuralNetwork:
     def predict(self, X):
         for i in range(len(self.layers)-1):
             if i == len(self.layers)-2:
-                X = self.__sigmoid(np.dot(X, self.__W[i]) + self.__b[i].T)
+                X = self.__output_activation(np.dot(X, self.__W[i]) + self.__b[i].T)
             else:
-                X = self.__activation(np.dot(X, self.__W[i]) + self.__b[i].T)
+                X = self.__hidden_activation(np.dot(X, self.__W[i]) + self.__b[i].T)
         return X
 
     def loss(self, y, ypred):
-        return np.mean(-(y*np.log(ypred) + (1-y)*np.log(1-ypred)))
+        epsilon = 0.0001#a number to advoid division by zero
+        return np.mean(-(y*np.log(ypred+epsilon) + (1-y)*np.log(1-ypred+epsilon)))
 
     def score(self, X, y):
-        sc = y == np.around(self.predict(X))
+        if self.layers[-1] > 1:
+            ypred = np.argmax(self.predict(X),axis=1).reshape(-1,1)
+        else:
+            ypred = np.around(self.predict(X))
+        sc = y == ypred
         return np.sum(sc) / len(sc)
 
 if __name__ == "__main__":
@@ -137,11 +181,11 @@ if __name__ == "__main__":
     y = data[:, d-1].reshape(-1,1)
 
     score = {}
-    for i in ['tanh', 'logistic', 'relu']:
+    for i in ['tanh', 'logistic', 'relu', 'elu', 'prelu']:
         sta = time.time()
-        mlp = NeuralNetwork([X.shape[1], 2, 1], 0.01, activation=i)
-        mlp.fit(X, y, 5000, 100)
-        score[i] = [mlp.score(X, y), time.time() - sta]
+        mlp = NeuralNetwork([X.shape[1], 3, 3, 1], 0.01, activation=i)
+        mlp.fit(X[:N-3], y[:N-3], 1000, 100)
+        score[i] = [mlp.score(X[N-3:], y[N-3:]), mlp.loss(y[N-3:], mlp.predict(X[N-3:])), time.time() - sta]
 
     for i in score:
-        print("%s activation function output score: %s - Training time: %ss" % (i, score[i][0], score[i][1]))
+        print("%s activation function output score: %s - Loss: %s - Training time: %ss" % (i, score[i][0], score[i][1], score[i][2]))
